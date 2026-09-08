a508512eba3e656f3c223bccb3d739ab4ba12510
Andrii Sachuk <ansachuk@icloud.com>
2026-09-08 15:35:06 +0200

    fixed new Random creation

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
     private ColorSupplier supplier = new ColorSupplier();
+    private Random random = new Random();
 
     public Ball getRandomBall() {
         String color = supplier.getRandomColor();
-        int number = new Random().nextInt(MAX_BALL_NUMBER);
+        int number = random.nextInt(MAX_BALL_NUMBER);
 
         return new Ball(color, number);
     }
